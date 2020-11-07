c4f8ab2e483b7def0ad758c491c67f5da80ad41e
Mica Massie <micamassie@gmail.com>
2020-11-07 11:02:02 -0600

    Add files via upload

---
+++ ufo-level1/js/app.js
@@ -0,0 +1,44 @@
+// from data.js
+var tableData = data;
+
+
+// Console.log the weather data from data.js
+console.log(data);
+
+function buildTable(data) {
+
+ tbody.html("");
+    data.forEach((dataRow) => {
+        var row=tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+            let cell=row.append("td");
+            cell.text(val);
+        }); 
+    });    
+}
+buildTable(tableData);
+
+var button = d3.select("filter-btn");
+
+var form = d3.select("table-area");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+
+function handleClick() {
+
+    const date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    
+   buildTable(filteredData);
+
+} 
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
